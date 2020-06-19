fa0e1e69a9fbd27442af4652dd1b502f07ea3c01
Nguyễn Bảo Long <nguyenbaolong@MEO-MEO.mshome.net>
2020-06-19 18:21:52 +0700

    sql, create database, demo

---
+++ src/main/java/com/hibernate/sql/QLSV.sql
@@ -0,0 +1,73 @@
+USE master
+GO
+
+DROP DATABASE QLSV
+GO
+
+CREATE DATABASE QLSV
+GO
+
+USE QLSV
+GO
+
+CREATE TABLE sinh_vien (
+    maSinhVien BIGINT,
+    mssv CHAR(20),
+    hoTen NVARCHAR(100),
+    gioiTinh NCHAR(3),
+    cmnd INT,
+    maLop BIGINT,
+
+    CONSTRAINT pk_sinhVien PRIMARY KEY(maSinhVien),
+    CONSTRAINT checkGioiTinh CHECK (gioiTinh IN ('Nam', 'nam', N'Nữ', N'nữ'))
+)
+GO
+
+CREATE TABLE mon_hoc (
+    maMonHoc BIGINT,
+    tenMonHoc NVARCHAR(100),
+
+    CONSTRAINT pk_monHoc PRIMARY KEY (maMonHoc)
+)
+GO
+
+CREATE TABLE lop_hoc (
+    maLop BIGINT,
+    tenLop VARCHAR(20),
+
+    CONSTRAINT pk_lopHoc PRIMARY KEY (maLop)
+)
+GO
+
+CREATE TABLE monHoc_lopHoc (
+    maMonHoc BIGINT,
+    maLop BIGINT,
+    phongHoc VARCHAR(20),
+
+    CONSTRAINT pk_monHoc_lopHoc PRIMARY KEY (maMonHoc, maLop)
+)
+GO
+
+CREATE TABLE sinhVien_monHoc (
+    maLop BIGINT,
+    maMonHoc BIGINT,
+    maSinhVien BIGINT,
+    diemGK FLOAT,
+    diemCK FLOAT,
+    diemChuyenCan FLOAT,
+    diemTong FLOAT,
+
+    CONSTRAINT pk_sinhVien_monHoc PRIMARY KEY (maLop , maMonHoc, maSinhVien)
+)
+GO
+
+ALTER TABLE monHoc_lopHoc ADD CONSTRAINT fk_monHocLopHoc_lopHoc FOREIGN KEY (maLop) REFERENCES lop_hoc(maLop)
+ALTER TABLE monHoc_lopHoc ADD CONSTRAINT fk_monHocLopHoc_monHoc FOREIGN KEY (maMonHoc) REFERENCES mon_hoc(maMonHoc)
+ALTER TABLE sinh_vien ADD CONSTRAINT fk_sinhVien_lopHoc FOREIGN KEY (maLop) REFERENCES lop_hoc(maLop)
+ALTER TABLE sinhVien_monHoc ADD CONSTRAINT fk_sinhVienMonHoc_monHocLopHoc FOREIGN KEY (maMonHoc, maLop) REFERENCES monHoc_lopHoc(maMonHoc, maLop)
+ALTER TABLE sinhVien_monHoc ADD CONSTRAINT fk_sinhVienHocMonHoc_sinhVien FOREIGN KEY (maSinhVien) REFERENCES sinh_vien(maSinhVien)
+GO
+
+
+
+
