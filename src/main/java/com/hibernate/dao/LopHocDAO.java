9cf36ba61560f57f28c1a468f9f21b7cd1daf234
Nguyễn Bảo Long <nguyenbaolong@MEO-MEO.mshome.net>
2020-06-20 19:54:23 +0700

    test save data to database

--- src/main/java/com/hibernate/dao/LopHocDAO.java
+++ src/main/java/com/hibernate/dao/LopHocDAO.java
@@ -1,5 +1,24 @@
 package com.hibernate.dao;
 
+import com.hibernate.entity.LopHoc;
+import com.hibernate.util.HibernateUtil;
+
+import org.hibernate.Session;
+import org.hibernate.SessionFactory;
+
 public class LopHocDAO {
-    
+    public static void addLopHoc(LopHoc l) {
+        SessionFactory sessionFactory = HibernateUtil.getSessionJavaConfigFactory();
+        Session session = sessionFactory.openSession();
+
+        session.beginTransaction();
+        session.save(l);
+        session.getTransaction().commit();
+    }
+
+    public static void main(String[] args) {
+        LopHoc l = new LopHoc();
+        l.set_tenLop("18CTT2");
+        LopHocDAO.addLopHoc(l);
+    }
 }
