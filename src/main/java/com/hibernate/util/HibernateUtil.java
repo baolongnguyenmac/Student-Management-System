4dbb5596ceb3c756276ec2f1e55e2a0590242975
Nguyễn Bảo Long <nguyenbaolong@MEO-MEO.mshome.net>
2020-06-20 19:55:06 +0700

    props.put(hibernate.id.new_generator_mappings, false);

--- src/main/java/com/hibernate/util/HibernateUtil.java
+++ src/main/java/com/hibernate/util/HibernateUtil.java
@@ -2,7 +2,7 @@ package com.hibernate.util;
 
 import java.util.Properties;
 
-// import com.hibernate.entity.*;
+import com.hibernate.entity.*;
 
 import org.hibernate.SessionFactory;
 import org.hibernate.boot.registry.StandardServiceRegistryBuilder;
@@ -20,31 +20,34 @@ public class HibernateUtil {
             // Create Properties, can be read from property files too
             Properties props = new Properties();
             props.put("hibernate.connection.driver_class", "com.microsoft.sqlserver.jdbc.SQLServerDriver");
-            props.put("hibernate.connection.url", "jdbc:sqlserver://localhost:1433;databaseName=testJava");
+            props.put("hibernate.connection.url", "jdbc:sqlserver://localhost:1433;databaseName=QLSV");
             props.put("hibernate.connection.username", "sa");
             props.put("hibernate.connection.password", "Long123ohio");
             props.put("hibernate.current_session_context_class", "thread");
             props.put("hibernate.dialect", "org.hibernate.dialect.SQLServerDialect");
+            props.put("hibernate.id.new_generator_mappings", "false");
 
             configuration.setProperties(props);
 
-            // // we can set mapping file or class with annotation
-            // // addClass(SinhVien.class) will look for resource SinhVien.hbm.xml (which is not good)
-            // configuration.addAnnotatedClass(SinhVien.class);
-            // configuration.addAnnotatedClass(MonHoc.class);
-            // configuration.addAnnotatedClass(LopHoc.class);
-            // configuration.addAnnotatedClass(SinhVienHocMonHoc.class);
-            // configuration.addAnnotatedClass(MonHocThuocLopHoc.class);
+            // we can set mapping file or class with annotation
+            // addClass(SinhVien.class) will look for resource SinhVien.hbm.xml (which is not good)
+            configuration.addAnnotatedClass(SinhVien.class);
+            configuration.addAnnotatedClass(MonHoc.class);
+            configuration.addAnnotatedClass(LopHoc.class);
+            configuration.addAnnotatedClass(SinhVien_MonHoc.class);
+            configuration.addAnnotatedClass(MonHoc_LopHoc.class);
 
             ServiceRegistry serviceRegistry = new StandardServiceRegistryBuilder().applySettings(configuration.getProperties()).build();
-            System.out.println("Hibernate Java Config serviceRegistry created");
+            // System.out.println("Hibernate Java Config serviceRegistry created");
+            System.out.println("Có vẻ đang ổn vãi lồn nhé");
 
             SessionFactory sessionFactory = configuration.buildSessionFactory(serviceRegistry);
 
             return sessionFactory;
         }
         catch (Throwable ex) {
-            System.err.println("Initial SessionFactory creation failed." + ex);
+            // System.err.println("Initial SessionFactory creation failed." + ex);
+            System.err.println("Ăn lồn ở dòng 49 file HibernateUtil");
             throw new ExceptionInInitializerError(ex);
         }
     }
